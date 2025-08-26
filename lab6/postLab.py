d9b7cf08fe1ddac23f68417b3817f8a965868082
Heer972005 <mehtaheer.972@gmail.com>
2025-08-26 17:48:12 +0530

    post lab

--- lab6/postLab.py
+++ lab6/postLab.py
@@ -26,3 +26,29 @@ first=n
 while(first>=10):
     first//=10
 print(f"Last digit:{last},first digit:{first}")
+
+n=10
+n=abs(n)
+last=n%10
+first=n
+while(first>=10):
+    first//=10
+temp=first
+first=last
+last=temp
+print(f"swapped:\nfirst:{first},last:{last}")
+
+n=12
+pro=1
+while n>0:
+    r=n%10
+    pro*=r
+    n//=10
+print(pro)
+
+n=23
+rev=0
+while n>0:
+    rev=rev*10+(n%10)
+    n//=10
+print(rev)
