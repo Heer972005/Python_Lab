ad3840767343696fc1ca4262597284061195db39
Heer972005 <mehtaheer.972@gmail.com>
2025-09-01 10:50:39 +0530

    Post Lab

--- lab15/sql.py
+++ lab15/sql.py
@@ -3,6 +3,8 @@ conn=sqlite3.connect('student_record.db')
 cursor=conn.cursor()
 print("Database connected successfully!")
 
+cursor.execute("DROP TABLE IF EXISTS student_record")
+#added this drop line as to exceute everytime it will nor show dupliacte error
 cursor.execute('''CREATE TABLE IF NOT EXISTS student_record( 
                Enrollment INTEGER PRIMARY KEY AUTOINCREMENT, 
                name TEXT NOT NULL, 
@@ -16,6 +18,7 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS student_record(
 #AUTOINCREMENT-Values are automatically assigned (1, 2, 3..)
 
 conn.commit()#stors permanantly
+
 
 student_record=[
     (92301733016,'ASHUTOSH KUMAR SINGH','PWP',95),
@@ -42,3 +45,22 @@ high_marks=cursor.fetchall()
 print("\n Students with Marks greater than 90:")
 for i in high_marks:
     print(i)
+
+cursor.execute('''UPDATE student_record SET Mark=98
+               WHERE name='ASHUTOSH KUMAR SINGH' AND subject='PWP' ''')
+conn.commit()
+cursor.execute('SELECT name,Mark FROM student_record WHERE name="ASHUTOSH KUMAR SINGH"')
+update_mark=cursor.fetchone()
+print(f"\nUpdate Mark for {update_mark[0]}:{update_mark[1]}")
+
+cursor.execute('''DELETE FROM student_record WHERE name='DEVENDRASINH DOLATSINH JADEJA' ''')
+conn.commit()
+cursor.execute('SELECT*FROM student_record WHERE name="DEVENDRASINH DOLATSINH JADEJA"')
+deleted_name=cursor.fetchone()
+if(deleted_name is None):
+    print("\n DEVENDRASINH DOLATSINH JADEJA has been succefully deleted.")
+
+cursor.execute('''SELECT AVG(Mark) FROM student_record''')
+avg_mark=cursor.fetchone()[0]
+print(f"\n The averge mark of student is:${avg_mark:.2f}")
+conn.close()
